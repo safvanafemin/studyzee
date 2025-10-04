7cf3f0681cb26fbf3c5ae4bb52aac32f41f794f7
hibafathy12 <hibaf1966@gamil.com>
2025-10-04 10:40:35 +0530

    create home

---
+++ lib/teacher/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StdHomeScreen extends StatefulWidget {
+  const StdHomeScreen({super.key});
+
+  @override
+  State<StdHomeScreen> createState() => _StdHomeScreenState();
+}
+
+class _StdHomeScreenState extends State<StdHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
